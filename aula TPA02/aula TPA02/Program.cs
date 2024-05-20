fad2d5c823b7a0a3367acaa2c332ab9d3d731751
Miguel-Molina0 <167210792+Miguel-Molina0@users.noreply.github.com>
2024-05-20 16:37:43 -0300

    Add files via upload

---
+++ aula TPA02/aula TPA02/Program.cs
@@ -0,0 +1,48 @@
+﻿double res;
+double n1;
+double n2;
+int opc;
+
+Console.WriteLine("digite o primeiro número");
+n1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("");
+
+
+Console.WriteLine("digite o segundo número");
+n2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("");
+
+Console.WriteLine("digite a operação que deseja");
+Console.WriteLine("1 - soma     2 - subtração     3 - multiplicação     4 - divisão");
+opc = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("");
+
+if (opc == 1)
+{
+    res = n1 + n2;
+    Console.WriteLine(res);
+}
+
+else if (opc == 2)
+{
+    res = n1 - n2;
+    Console.WriteLine(res);
+}
+
+else if (opc == 3)
+{
+    res = n1 * n2;
+    Console.WriteLine(res);
+}
+
+else if (opc == 4)
+{
+    res = n1 / n2;
+    Console.WriteLine(res);
+}
+
+else
+{
+    Console.WriteLine("invalido");
+}
+
